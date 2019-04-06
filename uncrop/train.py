7a2b90fd08a03ae026d6ca50dd583e5a1d53d05d
quasarbright <mthundershock@gmail.com>
2019-04-06 13:11:02 -0400

    train many configs in sequence and plot losses

--- uncrop/train.py
+++ uncrop/train.py
@@ -3,21 +3,27 @@ from torch import nn
 from load_data import get_dataloaders
 from model import UnCropper
 from utils import device
-def train_model(crop_ratio=.8, batch_size=500, lr=.3, momentum=.9, epochs=100):
+import matplotlib.pyplot as plt
+
+
+def train_model(crop_ratio=.8, batch_size=500, lr=.3, momentum=.9, epochs=100, num_kernels=5, kernel_size=6):
     # cifar
     print("loading data")
-    trainloader, testloader, uncropped_shape, cropped_shape = get_dataloaders(crop_ratio, batch_size)
+    trainloader, testloader, uncropped_shape, cropped_shape = get_dataloaders(
+        crop_ratio, batch_size)
     print("data loaded")
     img_width, img_height = uncropped_shape[3], uncropped_shape[2]
     img_size = (img_width, img_height)
     cropped_width, cropped_height = cropped_shape[3], cropped_shape[2]
     cropped_size = (cropped_width, cropped_height)
 
-    model = UnCropper(img_size, cropped_size).to(device)
+    model = UnCropper(img_size, cropped_size,
+                      num_kernels, kernel_size).to(device)
     loss_fn = nn.BCELoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=.1, momentum=.9)
 
-    print("beginning training with image size {} and cropped size {}".format(img_size, cropped_size))
+    print("beginning training with image size {} and cropped size {}".format(
+        img_size, cropped_size))
     losses = []
     for epoch in range(epochs):
         total_loss = 0
@@ -36,9 +42,56 @@ def train_model(crop_ratio=.8, batch_size=500, lr=.3, momentum=.9, epochs=100):
         avg_loss = total_loss / num_losses
         losses.append(avg_loss)
         print('loss at epoch {}: {}'.format(epoch, avg_loss))
-    print("training complete. final loss after {} epochs: {}".format(epochs, losses[-1]))
+    print("training complete. final loss after {} epochs: {}".format(
+        epochs, losses[-1]))
+    
     return model, losses
-            
+
+def train_many(*configs):
+    '''
+    trains all configs
+    configs is list of dictionaries with kwargs for training
+    plots losses
+    returns models and loss lists in order
+    '''
+    models = []
+    loss_lists = []
+    for config in configs:
+        # config is a dictionary of kwargs
+        print('training config: {}'.format(config))
+        model, losses = train_model(**config)
+        models.append(model)
+        loss_lists.append(losses)
+        plt.plot(losses)
+    plt.legend([str(config) for config in configs], loc='upper right')
+    plt.show()
+    return models, loss_lists
+
+
 
 if __name__ == '__main__':
-    train_model(.95)
+    configs = [
+        {
+            'num_kernels': 32,
+            'kernel_size': 7,
+            'crop_ratio': .95
+        },
+        {
+            'num_kernels': 32,
+            'kernel_size': 7,
+            'crop_ratio': .8
+        },
+        {
+            'num_kernels':32,
+            'kernel_size':7,
+            'crop_ratio':.95,
+            'momentum':.8,
+            'lr':.5
+        },
+        {
+            'num_kernels':64,
+            'kernel_size':7,
+            'crop_ratio':.95,
+        }
+    ]
+    train_many(*configs)
